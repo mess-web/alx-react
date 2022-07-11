89751e565bc20e7aa205516ad6a8e97e0a9a233a
sentayehut <tsentayehu4@gmail.com>
2022-07-11 04:42:39 -0700

    webpack

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: path.resolve(__dirname, 'js/dashboard_main.js'),
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+
+  performance: {
+    maxAssetSize: 1000000
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+
+      // {
+      //   test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+      //   use: [
+      //     'file-loader',
+      //     {
+      //       loader: 'image-webpack-loader',
+      //       options: {
+      //         disable: true
+      //       }
+      //     }
+      //   ]
+      // },
+
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
