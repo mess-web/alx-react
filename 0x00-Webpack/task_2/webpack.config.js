bea992ab650312d941e2ca35ee76fbb396e4481d
sentayehut <tsentayehu4@gmail.com>
2022-07-11 04:57:32 -0700

    webpack

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,45 +1,34 @@
 const path = require('path');
 
-
 module.exports = {
   mode: 'production',
-  entry: {
-    index: path.resolve(__dirname, 'js/dashboard_main.js'),
-  },
-
+  entry: './js/dashboard_main.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
-
   performance: {
-    maxAssetSize: 1000000
+    maxAssetSize: 1000000,
   },
-
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-
-      // {
-      //   test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-      //   use: [
-      //     'file-loader',
-      //     {
-      //       loader: 'image-webpack-loader',
-      //       options: {
-      //         disable: true
-      //       }
-      //     }
-      //   ]
-      // },
-
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource'
-      }
-    ]
-  }
-}
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
